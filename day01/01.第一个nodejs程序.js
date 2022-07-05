4a1c1cb11db4d1e2165025234494af8353efc10e
TaoLi19970428 <1170272147@qq.com>
2022-07-05 15:34:10 +0800

    feat:(nodejs day01)

--- day01/01.第一个nodejs程序.js
+++
@@ -1,4 +0,0 @@
-console.log("淡黄的长裙~");
-console.log(1 + 1);
-const fn = () => console.log("蓬松的头发~");
-fn();
--- day01/02.global.js
+++ day01/02.global.js
@@ -1,2 +1,9 @@
+// global 作为 nodejs 的顶层对象（在nodejs环境运行）
+// global 可以省略
 console.log(global);
+// console.log(global.process);
+// console.log(global.Buffer);
+console.log(Buffer);
+
+// nodejs 中没有 window 对象
 // console.log(window);//报错
