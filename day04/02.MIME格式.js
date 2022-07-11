b0f62f17fe6ed0e88fceea5ec73ffb8376c25c1a
TaoLi19970428 <1170272147@qq.com>
2022-07-11 09:43:20 +0800

    feat:nodejs:day04

---
+++ day04/02.MIME格式.js
@@ -0,0 +1,18 @@
+/* 
+    MIME类型：
+        text/plain   (文本)
+        text/css     (css)
+        text/html    (html)
+        application/javascript  (js)
+        application/json        (json)
+        application/x-www-form-urlencoded   (form表单类型)
+
+        image/gif    (图片)
+        image/jpeg
+        image/png
+        image/webp
+        image/svg+xml
+
+        audio/mp3    (音频)
+        video/mp4    (视频)
+*/
